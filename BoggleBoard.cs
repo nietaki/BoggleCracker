556a2f09f3e1167c646dab9503a4d836ac2e456b
nietaki <nietaki@gmail.com>
2016-01-16 14:23:59 +0000

    BoggleBoard sort of done

--- BoggleBoard.cs
+++ BoggleBoard.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BoggleCracker
 {
 	public class BoggleBoard
 	{
 		private string letters;
+		private bool[] isUsed;
 		
 		public BoggleBoard(string rows)
 		{
@@ -12,12 +15,72 @@ namespace BoggleCracker
 				throw new ArgumentException("boggle board consists of 16 characters", rows);
 			}
 			this.letters = rows.ToLower();
+			this.isUsed = new bool[16];
+		}
+
+		public char GetCharacter(int idx) {
+			return letters[idx];
 		}
 
 		public void PrintRepresentation() {
 			for (int i = 0; i < 4; i++) {
 				Console.WriteLine(letters.Substring(i * 4, 4));
 			}
+		}
+
+		private void SetUsageState(int idx, bool usageState) {
+			if (this.isUsed[idx] == usageState) {
+				throw new ArgumentException("trying to use/unuse an already used/unused field");
+			}
+			this.isUsed[idx] = usageState;
+		}
+
+		public void Use(int idx) {
+			this.SetUsageState(idx, true);
+		}
+
+		public void Unuse(int idx) {
+			this.SetUsageState(idx, false);
+		}
+
+		public List<int> GetNeighbors(int idx) {
+			List<int> indices = new List<int>();	
+			indices.Add(idx - 4);
+			indices.Add(idx + 4);
+
+			if (!IsOnLeftWall(idx)) {
+				indices.Add(idx - 5);
+				indices.Add(idx - 1);
+				indices.Add(idx + 3);
+			}
+
+			if (!IsOnRightWall(idx)) {
+				indices.Add(idx - 3);
+				indices.Add(idx + 1);
+				indices.Add(idx + 5);
+			}
+
+			return indices.Where(this.IsWithinBoard).ToList();
+		}
+
+		public List<int> GetUnusedNeighbors(int idx) {
+			return this.GetNeighbors(idx).Where(this.IsUnusedAndWithinBoard).ToList();
+		}
+
+		private static bool IsOnLeftWall(int idx) {
+			return (idx % 4) == 0;
+		}
+
+		private static bool IsOnRightWall(int idx) {
+			return (idx % 4) == 3;
+		}
+
+		public bool IsWithinBoard(int idx) {
+			return (idx >= 0) && (idx < 4 * 4);
+		}	
+
+		public bool IsUnusedAndWithinBoard(int idx) {
+			return this.IsWithinBoard(idx) && !this.isUsed[idx];
 		}
 	}
 }
